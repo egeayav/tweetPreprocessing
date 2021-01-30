212f94f5c04eb8beae9b4f191117e7d61a3f1c47
egeayav <78220257+egeayav@users.noreply.github.com>
2021-01-30 14:01:49 +0300

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+consumer_secret= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token= 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token_secret= 'xxxxxxxxxxxxxxxxxxxxxxxxxx'
